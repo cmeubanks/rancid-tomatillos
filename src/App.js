a7cddc0f19d6e52c44bc05bd5dee21d239895de2
Caroline Eubanks <cmeubanks@gmail.com>
2021-05-12 16:06:04 -0600

    created new files and imported movie data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import movieData from './movieData.js';
+// import MovieLibrary from './components/MovieLibrary.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      movies: {movieData}
+    }
+  }
+  //mapping over movieData here to create prop for MovieLibrary
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
