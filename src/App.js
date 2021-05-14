695aa85e283bb05c2966ee8646dd3cf579b2d458
Caroline Eubanks <cmeubanks@gmail.com>
2021-05-13 20:42:33 -0600

    set state bug

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
 import React, { Component } from 'react';
 import movieData from './movieData.js';
+// import flick from './flicks';
 import Header from './components/Header.js';
 import MovieLibrary from './components/MovieLibrary.js';
+import MovieInfo from './components/MovieInfo.js';
 
 import './index.scss';
-
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
       movies: {movieData},
-      cardId: 0
+      newMovie: null,
     }
   }
 
   handleClick = (e) => {
-    this.setState({ cardId: e.target.id })
+    let idNum = parseInt(e.target.id);
 
-    return console.log(e.target.id);
+    let match = this.state.movies.movieData.movies.find(elem => {
+      return elem.id === idNum;
+    });
+
+    this.setState({ newMovie: match});
+
+    console.log("STATE", this.state);
   }
 
   render() {
     return (
       <div className='main-page'>
         <Header />
-        {!this.state.cardId && <MovieLibrary movies={this.state.movies} handleClick = {this.handleClick}/>}
+        {!this.state.newMovie && <MovieLibrary movies={this.state.movies} handleClick={this.handleClick}/>}
+        {this.state.newMovie && <MovieInfo movie={this.newMovie} />}
       </div>
     );
   }
