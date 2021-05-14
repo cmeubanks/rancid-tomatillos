17093a691a392235844872896d1999d30263800c
Caroline Eubanks <cmeubanks@gmail.com>
2021-05-14 16:52:39 -0600

    individual movie api

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -5,6 +5,12 @@ const apiCalls = {
     const data = await response.json();
     return data;
   }
+
+  async fetchAMovie(id) {
+    const response = await fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`);
+    const data = await response.json();
+    return data;
+  }
 }
 
 export default apiCalls;
