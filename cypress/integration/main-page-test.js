f47e61bf83d486d8e02149ff46f380c314b24e8b
Caroline Eubanks <cmeubanks@gmail.com>
2021-05-20 17:37:30 -0600

    completed main page testing

--- cypress/integration/main-page-test.js
+++ cypress/integration/main-page-test.js
@@ -1,6 +1,7 @@
 describe('Main', () => {
 
   before(() => {
+    // cy.visit('http://localhost:3000')
     cy.fixture('../fixtures/movies-data.json')
       .then((movies) => {
       cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
@@ -8,19 +9,31 @@ describe('Main', () => {
         statusCode: 200,
       });
     });
+    cy.visit('http://localhost:3000')
   });
 
   it('Should display a loading page while fetching movie data', () => {
-    cy.visit('http://localhost:3000')
-    .get('.message').should('contain', 'Page Loading 🍿')
+    cy.get('.message').should('contain', 'Page Loading 🍿')
       .should('be.visible')
   });
 
+  it('Should remove loading message once movies have loaded', () => {
+    cy.get('.message').should('not.exist')
+  });
+
   it('Should render the main page', () => {
-    cy.visit('http://localhost:3000')
-      .get('h1').contains('Rancid Tomatillos')
+    cy.get('h1').contains('Rancid Tomatillos')
       .get('#694919')
       .get('img').should('have.attr', 'src', 'https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg')
+  });
+
+  it('Should display all movies in the fetched dataset', () => {
+    cy.get('.library')
+      .find('.movie-card').should('have.length', 1)
+  });
+
+  it('Should display correct path on page load', () => {
+    cy.url().should('eq', 'http://localhost:3000/')
   });
 
 });
