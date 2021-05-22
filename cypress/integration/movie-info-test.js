dc465e1001fcd00cb7dcacfbe0221de49f26b3a2
pcmueller <pmuellerleile@gmail.com>
2021-05-21 21:07:25 -0600

    Built commands.js and refactored all testing

--- cypress/integration/movie-info-test.js
+++ cypress/integration/movie-info-test.js
@@ -1,39 +1,28 @@
-describe('Movie Info Loading', () => {
+describe('Movie Info Page', () => {
 
-  it('Should display a loading page while fetching movie data', () => {
+  beforeEach(() => {
+    // see: cypress/support/commands.js
+    cy.stubSingleMovieData()
+  });
 
-    cy.fixture('../fixtures/single-movie-data.json')
-      .then((movie) => {
-        cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919', {
-          body: movie,
-          delay: 100,
-          statusCode: 200,
-        });
-        cy.visit('http://localhost:3000/694919')
-      });
+  describe('Movie Info Loading', () => {
 
-      cy.wait(150).get('.message').should('contain', 'Page Loading 🍿')
+    it('Should display a loading page while fetching movie data', () => {
+      cy.get('.message')
+        .should('contain', 'Page Loading 🍿')
         .should('be.visible')
-      cy.wait(150).reload()
+    });
+
+    it('Should remove loading message once movies have loaded', () => {
+      cy.get('.message').should('not.exist')
+    });
+
   });
 
-});
+  describe('Movie Info Display', () => {
 
-describe('Movie Info Display', () => {
-
-  before(() => {
-    cy.fixture('../fixtures/single-movie-data.json')
-      .then((movie) => {
-        cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919', {
-          body: movie,
-          statusCode: 200,
-        });
-      });
-    cy.visit('http://localhost:3000/694919')
-  });
-
-  it('Should render the movie info display', () => {
-    cy.get('.title').contains('Money Plane')
+    it('Should render all data for the movie info display', () => {
+      cy.get('.title').contains('Money Plane')
       .get('img').should('have.attr', 'src', 'https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg')
       .get('section').should('have.css', 'background-image', 'url("https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg")')
       .get('.release-date').contains('September 29, 2020')
@@ -44,35 +33,23 @@ describe('Movie Info Display', () => {
       .get('.runtime').contains(82)
       .get('.tagline').eq('')
       .get('.avg-rating').contains(6)
-  });
-
-});
-
-describe('Home Click', () => {
-
-  it('Should go back to main page view when the Return Home button is clicked', () => {
-
-    cy.fixture('../fixtures/single-movie-data.json')
-      .then((movie) => {
-      cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919', {
-        body: movie,
-        delay: 300,
-        statusCode: 200,
-      });
-      cy.visit('http://localhost:3000/694919')
     });
 
-    cy.fixture('../fixtures/movies-data.json')
-      .then((movies) => {
-        cy.intercept('https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
-          body: movies,
-          delay: 300,
-          statusCode: 200,
-        });
-        cy.wait(300).get('button').click().reload()
-          .url().should('eq', 'http://localhost:3000/')
-          .get('.library').find('.movie-card').should('have.length', 1)
-      });
+  });
+
+  describe('Return Home Click', () => {
+
+    before(() => {
+      cy.interceptAllMoviesData()
+    });
+
+    it('Should go back to main page view when the Return Home button is clicked', () => {
+
+      cy.get('button').click()
+        .url().should('eq', 'http://localhost:3000/')
+        .get('.library').find('.movie-card').should('have.length', 1)
+    });
+
   });
 
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,22 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import allMoviesData from '../fixtures/movies-data.json';
+import singleMovieData from '../fixtures/single-movie-data.json';
+
+const baseURL = 'https://rancid-tomatillos.herokuapp.com/api/v2/movies'
+
+Cypress.Commands.add('stubAllMoviesData', () => {
+  cy.intercept(baseURL, allMoviesData)
+    .visit('http://localhost:3000/')
+})
+
+Cypress.Commands.add('stubSingleMovieData', () => {
+  cy.intercept(`${baseURL}/694919`, singleMovieData)
+    .visit('http://localhost:3000/694919')
+});
+
+Cypress.Commands.add('interceptAllMoviesData', () => {
+  cy.intercept(baseURL, allMoviesData)
+})
+
+Cypress.Commands.add('interceptSingleMovieData', () => {
+  cy.intercept(`${baseURL}/694919`, singleMovieData)
+});
